6b4884c2b662444b41ee4d7341c68a2f200a1b30
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-03 00:21:50 +0530

    Added a code for calculating average of 2 numbers.

---
+++ avg.py
@@ -0,0 +1,8 @@
+# A program for finding avg of two numbers.
+
+#Taking input from user
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+#Finding average
+print("The average of the two numbers is: ",(a+b)/2)
