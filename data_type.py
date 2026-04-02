ba95706e04b032a40adc4b8667a978d53be810eb
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-03 00:05:54 +0530

    Added a code for identifying data type.

---
+++ data_type.py
@@ -0,0 +1,11 @@
+# A code for finding the data type of a variable.
+
+#integer
+a = int(input("Eneter any integer: "))
+print(type(a))
+#float
+a1 = float(input("Enter any decimal number: "))
+print(type(a1))
+#string
+a2 = input("Enter any word or sentence: ")
+print(type(a2))
