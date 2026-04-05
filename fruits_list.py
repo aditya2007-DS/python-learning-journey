b74313b9f90dadfe2ffe395467996ca8c223efa2
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-05 23:53:56 +0530

    Added a code to make a list of fruits.

---
+++ fruits_list.py
@@ -0,0 +1,19 @@
+# A program to store seven fruits in a list enterd by the user.
+
+fruits = []
+
+f1 = input("Enter a fruit name: ")
+fruits.append(f1)
+f2 = input("Enter a fruit name: ")
+fruits.append(f2)
+f3 = input("Enter a fruit name: ")
+f4 = input("Enter a fruit name: ")
+fruits.append(f4)
+f5 = input("Enter a fruit name: ")
+fruits.append(f5)
+f6 = input("Enter a fruit name: ")
+fruits.append(f6)
+f7 = input("Enter a fruit name: ")
+fruits.append(f7)
+
+print(fruits)
