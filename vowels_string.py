a63486773598a29e095c172a2bc8940eca127595
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-10 23:04:28 +0530

    Added a code to find vowels in a string.

---
+++ vowels_string.py
@@ -0,0 +1,12 @@
+# A code to count in the string.
+
+text = input("Enter a string: ")
+
+vowels = "aeiouAEIOU"
+count = 0
+
+for char in text:
+    if char in vowels:
+        count += 1
+
+print("Number of vowels:", count)
