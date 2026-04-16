dd99538895a991a32bd2ace9ee190893dce34755
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-16 23:46:10 +0530

    Added a code to get the initials of a name.

---
+++ initials.py
@@ -0,0 +1,11 @@
+# A code to get initials of name
+
+name = input("Enter your full name: ")
+
+words = name.split()
+initials = ""
+
+for word in words:
+    initials += word[0].upper()
+
+print("Initials:", initials)
