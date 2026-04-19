e5531941cf441990fc684163382425202eb560b6
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-19 23:28:29 +0530

    Added a prime number checker code.

---
+++ prime_no_checker.py
@@ -0,0 +1,13 @@
+# A code to check whether a number is prime or not. 
+
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a Prime Number")
+            break
+    else:
+        print("Prime Number")
+else:
+    print("Not a Prime Number")
