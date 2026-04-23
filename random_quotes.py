7621eae9bcb3d5a759a861d52d018a24a6458c5c
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-23 23:33:14 +0530

    Added a code to generate a random quote.

---
+++ random_quotes.py
@@ -0,0 +1,11 @@
+# A code to generate a random quote.
+
+import random
+
+quotes = [
+    "Stay hungry, stay foolish.",
+    "Code never lies, comments sometimes do.",
+    "Push yourself, because no one else will."
+]
+
+print(random.choice(quotes))
