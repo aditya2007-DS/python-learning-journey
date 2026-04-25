b0c1c04c6882579765c37d1afbd84c8c7095e8fb
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-25 20:35:33 +0530

    A code to translate few words from Hindi to English.

---
+++ dict_translate.py
@@ -0,0 +1,7 @@
+# A code to translate few words from Hindi to English.
+
+words = { "madad": "Help", "kursi": "Chair", "billi": "Cat"}
+
+word = input("Enter the word you want to translate: ")
+
+print(words[word])
