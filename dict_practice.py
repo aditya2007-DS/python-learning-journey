c09d85a95c9566803af0a2dc25bdf7d0bacd2323
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-26 18:43:07 +0530

    Update method practice.

---
+++ dict_practice.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter your friend's name: ")
+lang = input("Enter their favorite language: ")
+d.update({name: lang})
+
+name = input("Enter your friend's name: ")
+lang = input("Enter their favorite language: ")
+d.update({name: lang})
+
+name = input("Enter your friend's name: ")
+lang = input("Enter their favorite language: ")
+d.update({name: lang})
+
+name = input("Enter your friend's name: ")
+lang = input("Enter their favorite language: ")
+d.update({name: lang})
+
+print(d)
