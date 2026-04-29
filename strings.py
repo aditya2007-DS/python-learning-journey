9bb6bec8b5fd67757668b2a2ac4f840928010068
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-29 18:32:22 +0530

    Added a code of equality check of two strings.

---
+++ strings.py
@@ -0,0 +1,10 @@
+# A code of equality check of two strings.
+
+s1 = input("Enter string 1: ")
+s2 = input("Enter string 2: ")
+
+if (s1 == s2):
+    print("Strings are equal.")
+
+else:
+    print("Strings are not equal.")
