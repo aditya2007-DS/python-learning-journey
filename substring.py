6f72057931813bf556c7020f96bae50bd2de7680
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-29 18:35:24 +0530

    Added a code to check substring.

---
+++ substring.py
@@ -0,0 +1,9 @@
+# A code to check substring.
+
+s1 = input("Enter string1: ")
+s2 = input("Enter string2: ")
+
+if s2 in s1:
+    print("Substring is found.")
+else:
+    print("Substring not found.")
