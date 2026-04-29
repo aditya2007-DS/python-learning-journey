461db07e3ba73c75be4f9a6e1864b3b73e351fb6
Aditya Kamble <aditya.hk2007@gmail.com>
2026-04-29 19:51:31 +0530

    A code to check whether a name is in the list or not.

---
+++ find_name.py
@@ -0,0 +1,11 @@
+# A code to check whether a name is in a list or not.
+
+l = ["Aditya", "Ritesh", "Akash", "Harry"]
+
+name = input("Enter you name: ")
+
+if(name in l):
+    print("Your name is in the list!")
+
+else:
+    print("Your name is not in the list!")
